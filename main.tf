f075d6134e22303a7f8b7b7b1fe522e779cbd25d
adelinesharla <adelinesharla@duck.com>
2024-09-30 20:44:44 -0300

    fix: replace POST with GET

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_api_gateway_resource" "authentication_resource" {
 resource "aws_api_gateway_method" "authentication_method" {
   rest_api_id   = aws_api_gateway_rest_api.authentication_api.id
   resource_id   = aws_api_gateway_resource.authentication_resource.id
-  http_method   = "POST"
+  http_method   = "GET"
   authorization = "NONE" 
 }
 
